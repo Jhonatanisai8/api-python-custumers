10ff67d03420a6dbb292a5882a510092bb18b5bd
Jhonatan <ojedasnachezjhonatan@gmail.com>
2025-01-31 15:44:37 -0500

    definicion de los metodos guardar, eliminar, obtener

--- main.py
+++ main.py
@@ -2,6 +2,22 @@ from flask import Flask,render_template
 
 app = Flask(__name__)
 
+@app.route('/custumers',methods=['POST'])
+def save_custumer():
+    return "ok save"
+
+@app.route('/custumers/<int:id>',methods=['DELETE'])
+def remove_custumer(id):
+    return "ok remove"
+
+@app.route('/custumers/<int:id>')
+def get_custumer(id):
+    return "ok get"
+
+@app.route('/custumers')
+def get_all_custumer():
+    return "ok get"
+
 @app.route('/holaMundo')
 def index():
     return render_template("index.html")
