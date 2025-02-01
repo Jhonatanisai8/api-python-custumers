45570788346d823d64d263a923b8033fd5fe523a
Jhonatan <ojedasnachezjhonatan@gmail.com>
2025-01-31 22:44:49 -0500

    peticiones crud

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
 from flask_mysqldb import MySQL
 
 # configuracion con la base de datos
@@ -6,31 +6,62 @@ app = Flask(__name__)
 app.config['MYSQL_HOST'] = 'localhost'
 app.config['MYSQL_USER'] = 'root'
 app.config['MYSQL_PASSWORD'] = 'admin123'
-app.config['MYSQL_DB'] = 'users'
+app.config['MYSQL_DB'] = 'users_bd'
 
-#objeto de mysql
+# objeto de mysql
 mysql = MySQL(app)
+
+
 @app.route('/custumers', methods=['POST'])
 def save_custumer():
     cur = mysql.connection.cursor()
-    cur.execute('insert into customers values (%s)',
-                (request.form['firstname'],))
-    return "ok save"
+    cur.execute(
+        "INSERT INTO custumers (firstname, lastname, email, phone, address) VALUES ('Jhonatan', 'Ojeda Sanchez', 'jhona@gmail.com', '963258741', 'Peru')")
+    mysql.connection.commit()
+    return "ok save custumer"
 
 
 @app.route('/custumers/<int:id>', methods=['DELETE'])
 def remove_custumer(id):
-    return "ok remove"
+    cur = mysql.connection.cursor()
+    cur.execute(
+        "DELETE FROM custumers WHERE custumer_id = %s", )
+    mysql.connection.commit()
+    return "ok remove custumer by id ", id
 
 
 @app.route('/custumers/<int:id>')
 def get_custumer(id):
-    return "ok get"
+    cur = mysql.connection.cursor()
+    cur.execute(
+        "SELECT custumer_id,firstname,lastname,email,phone, address FROM custumers WHERE custumer_id = " + str(id))
+    row = cur.fetchone()
+    content = {"custumer_id": row[0],
+               "firstname": row[1],
+               "lastname": row[2],
+               "email": row[3],
+               "phone": row[4],
+               "address": row[5]}
+    return jsonify(content)
 
 
 @app.route('/custumers')
 def get_all_custumer():
-    return "ok get"
+    cur = mysql.connection.cursor()
+    cur.execute("SELECT custumer_id,firstname,lastname,email,phone, address FROM custumers")
+    data = cur.fetchall()
+    result = []
+    for row in data:
+        content = {
+            "custumer_id": row[0],
+            "firstname": row[1],
+            "lastname": row[2],
+            "email": row[3],
+            "phone": row[4],
+            "address": row[5],
+        }
+        result.append(content)
+    return jsonify(result)
 
 
 @app.route('/holaMundo')
